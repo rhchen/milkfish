c5442464c96980b40fb19289f90b8f49a3998e39
ranhow.chen <ranhow.chen@gmail.com>
2014-02-23 17:54:09 +0800

    Add mvn parent and test plugin
    	new file:   trace/net.sf.milkfish.parent/.classpath
    	new file:   trace/net.sf.milkfish.parent/.project
    	new file:   trace/net.sf.milkfish.parent/.settings/org.eclipse.jdt.core.prefs
    	new file:   trace/net.sf.milkfish.parent/.settings/org.eclipse.m2e.core.prefs
    	new file:   trace/net.sf.milkfish.parent/META-INF/MANIFEST.MF
    	new file:   trace/net.sf.milkfish.parent/build.properties
    	renamed:    trace/net.sf.milkfish.product/pom.xml -> trace/net.sf.milkfish.parent/pom.xml
    	new file:   trace/net.sf.milkfish.product.tests/.classpath
    	new file:   trace/net.sf.milkfish.product.tests/.gitignore
    	new file:   trace/net.sf.milkfish.product.tests/.project
    	new file:   trace/net.sf.milkfish.product.tests/.settings/org.eclipse.jdt.core.prefs
    	new file:   trace/net.sf.milkfish.product.tests/.settings/org.eclipse.m2e.core.prefs
    	new file:   trace/net.sf.milkfish.product.tests/META-INF/MANIFEST.MF
    	new file:   trace/net.sf.milkfish.product.tests/build.properties
    	new file:   trace/net.sf.milkfish.product.tests/pom.xml
    	new file:   trace/net.sf.milkfish.product.tests/src/net/sf/milkfish/product/tests/AboutHandlerTest.java
    	modified:   trace/net.sf.milkfish.product/pom.xml

---
+++ trace/net.sf.milkfish.product.tests/src/net/sf/milkfish/product/tests/AboutHandlerTest.java
@@ -0,0 +1,58 @@
+/**
+ * 
+ */
+package net.sf.milkfish.product.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author admin
+ *
+ */
+public class AboutHandlerTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link net.sf.milkfish.product.handlers.AboutHandler#execute(org.eclipse.swt.widgets.Shell)}.
+	 */
+	@Test
+	public final void testExecute() {
+		
+		assertNotEquals(1, 2);
+	
+	}
+
+}
