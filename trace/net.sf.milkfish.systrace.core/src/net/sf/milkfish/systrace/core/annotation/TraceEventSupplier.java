4ac8687a4e9a10834ad8613ef6a99cee5a568bb7
ranhow.chen <ranhow.chen@gmail.com>
2014-03-27 22:56:46 +0800

    Add custom annotation
    	modified:   trace/net.sf.milkfish.feature/feature.xml
    	modified:   trace/net.sf.milkfish.systrace.core/.project
    	modified:   trace/net.sf.milkfish.systrace.core/META-INF/MANIFEST.MF
    	new file:   trace/net.sf.milkfish.systrace.core/OSGI-INF/component.xml
    	modified:   trace/net.sf.milkfish.systrace.core/build.properties
    	new file:   trace/net.sf.milkfish.systrace.core/src/net/sf/milkfish/systrace/core/annotation/TraceEvent.java
    	new file:   trace/net.sf.milkfish.systrace.core/src/net/sf/milkfish/systrace/core/annotation/TraceEventSupplier.java
    	modified:   trace/net.sf.milkfish.systrace.core/src/net/sf/milkfish/systrace/core/service/impl/SystraceService.java
    	modified:   trace/net.sf.milkfish.target/milkfish-e4.3.target

---
+++ trace/net.sf.milkfish.systrace.core/src/net/sf/milkfish/systrace/core/annotation/TraceEventSupplier.java
@@ -0,0 +1,53 @@
+package net.sf.milkfish.systrace.core.annotation;
+
+import org.eclipse.e4.core.di.suppliers.ExtendedObjectSupplier;
+import org.eclipse.e4.core.di.suppliers.IObjectDescriptor;
+import org.eclipse.e4.core.di.suppliers.IRequestor;
+import org.eclipse.linuxtools.tmf.core.event.ITmfEvent;
+import org.eclipse.linuxtools.tmf.core.event.ITmfEventField;
+import org.eclipse.linuxtools.tmf.core.event.TmfEvent;
+import org.eclipse.linuxtools.tmf.core.event.TmfEventField;
+import org.eclipse.linuxtools.tmf.core.event.TmfEventType;
+import org.eclipse.linuxtools.tmf.core.timestamp.TmfTimestamp;
+
+public class TraceEventSupplier extends ExtendedObjectSupplier {
+
+	private final String fContext = "UnitTest";
+    private final String fTypeId2 = "Some type2";
+
+    private final String   fLabel0 = "label1";
+    private final String   fLabel1 = "label2";
+    private final String   fLabel2 = "label3";
+    private final String[] fLabels = new String[] { fLabel0, fLabel1, fLabel2 };
+
+    private final TmfTimestamp fTimestamp3 = new TmfTimestamp(12355, (byte) 2, 5);
+
+    private final String fSource = "Source";
+
+    private final TmfEventType fType3 = new TmfEventType(fContext, fTypeId2, TmfEventField.makeRoot(fLabels));
+
+    private final String fReference = "Some reference";
+
+    @Override
+	public Object get(IObjectDescriptor descriptor, IRequestor requestor,
+			boolean track, boolean group) {
+		
+    	/**
+         * Object suppliers (primary & extended) are provide object describing the injection site 
+         * 
+         * IObjectDescriptor: provides access to the type and annotations of the field/parameter 
+         * 
+         * IRequestor: the requesting location: the method, field, constructor; can re-trigger
+         */
+    	
+    	String str = requestor.getRequestingObjectClass().getCanonicalName();
+    	
+		System.out.println("TraceEventSupplier.get "+ track);
+		
+		TmfEventField fContent3 = new TmfEventField(ITmfEventField.ROOT_FIELD_ID, "Some other different content", null);
+		ITmfEvent fEvent3 = new TmfEvent(null, fTimestamp3, fSource, fType3, fContent3, fReference);
+        
+		return fEvent3;
+	}
+
+}
--- trace/net.sf.milkfish.systrace.core/src/net/sf/milkfish/systrace/core/service/impl/SystraceService.java
+++ trace/net.sf.milkfish.systrace.core/src/net/sf/milkfish/systrace/core/service/impl/SystraceService.java
@@ -1,10 +1,13 @@
 package net.sf.milkfish.systrace.core.service.impl;
 
+import javax.inject.Inject;
 import javax.inject.Singleton;
 
+import net.sf.milkfish.systrace.core.annotation.TraceEvent;
 import net.sf.milkfish.systrace.core.service.ISystraceService;
 
 import org.eclipse.e4.core.di.annotations.Creatable;
+import org.eclipse.linuxtools.tmf.core.event.ITmfEvent;
 
 import com.fasterxml.jackson.core.JsonFactory;
 
@@ -16,5 +19,11 @@ public class SystraceService implements ISystraceService{
 		
 		return 0;
 		
+	}
+	
+	@Inject
+	public void annotate(@TraceEvent ITmfEvent event){
+		
+		System.out.println("SystraceService.annotate");
 	}
 }
